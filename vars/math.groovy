8a3926e2a77c5846c2faa19d7b6b6867036417e2
vishnu6400 <131434435+vishnu6400@users.noreply.github.com>
2023-05-02 11:33:46 +0530

    Create math.groovy

---
+++ vars/math.groovy
@@ -0,0 +1,19 @@
+def sum(a,b)
+{
+ echo "that sum is $(a) and $(b) is $(a+b)"
+}
+
+def sub(a,b)
+{
+  echo  "that sub is $(a) and $(b) is $(a-b)"
+}
+
+def mul(a,b)
+{
+ echo  "that product is $(a) and $(b) is $(a*b)"
+}
+
+def dev(a,b)
+{
+  echo "that devision is $(a) and $(b) is $(a/b)"
+}
