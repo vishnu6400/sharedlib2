e7a639fcb0843e13f5889cfc42421474cce45635
vishnu6400 <131434435+vishnu6400@users.noreply.github.com>
2023-05-02 13:16:26 +0530

    Update cicd.groovy

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -1,11 +1,13 @@
-def newgit(repo)
+def newGit(repo)
 {
   git "https://github.com/intelliqittrainings/${repo}.git"
 }
+
 def newMaven()
 {
   sh "mvn package"
 }
+
 def newDeploy(jobname,ipaddress,context)
 {
   sh "scp /var/lib/jenkins/workspace/${jobname}/webapp/target/webapp.war ubuntu@${ipaddress}:/var/lib/tomcat9/webapp/${context}.war"
